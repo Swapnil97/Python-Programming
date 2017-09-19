680f07b293b502145b2d223e2129ef1a029dc77d
swapnilmca17 <31646351+swapnilmca17@users.noreply.github.com>
2017-09-19 06:14:20 +0000

    Add files via upload

---
+++ sortTheList.py
@@ -0,0 +1,35 @@
+def miniIndex(List,lower,upper):
+    '''
+    OBJECTIVE:TO FIND THE INDEX OF MINIMUM ELEMENT OF THE LIST
+    INPUTS:1.List-LIST INPUTTED BY THE USER
+    RETURN:INDEX OF MINIMUM ELEMENT
+    '''
+    '''
+    APPROACH:BY USING RECURSION
+    '''
+    if lower==upper:
+        return upper
+    elif List[lower]<List[upper]:
+        return miniIndex(List,lower,upper-1)
+    else:
+        return miniIndex(List,lower+1,upper)
+def sortTheList(List,start=0):
+    '''
+    OBJECTIVE:TO SORT THE LIST BY FINDING MINIMUM ELEMENT USING FUNCTION
+    INPUTS:List=INPUTTED LIST BY USER
+    RETURN:SORTED LIST
+    '''
+    '''
+    APPROACH:BY USING RECURSION AND RECURSIVE FUNCTION miniIndex
+    '''
+    end=len(List)-1
+    if(start<=end):
+        minimum=miniIndex(List,start,end)
+        temp=List[start]
+        List[start]=List[minimum]
+        List[minimum]=temp
+        start=start+1
+        sortTheList(List,start)
+    return List
+L=[3,5,7,9,4,-1,6]
+sortTheList(L)
