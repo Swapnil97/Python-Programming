1217e60acb50cb686135f43567a4b14bd4bc6ebb
swapnilmca17 <31646351+swapnilmca17@users.noreply.github.com>
2017-10-10 13:17:22 +0530

    Add files via upload

---
+++ Flatter.py
@@ -0,0 +1,17 @@
+def Flatter(List,Flattered=[]):
+    '''
+    OBJECTIVE:TO flat the nested list inputted by user
+    INPUTS: 1.List-Given List
+            2.Flattered=new flattered list
+    Return: New FLattered list
+    '''
+    if(len(List)==0):
+        return Flattered
+    elif(type(List[0])== list):
+         Flattered.extend(List[0])     
+         return Flatter(List[1:],Flattered)
+    else:
+        Flattered.append(List[0])
+        return Flatter(List[1:],Flattered)
+List1=[1,2,43,67,[1,7,9]]
+print(Flatter(L))
