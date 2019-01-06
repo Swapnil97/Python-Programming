ea266b4731437aa7476b76a9b3a7a1dd76c30d83
Swapnil Madaan <31646351+Swapnil97@users.noreply.github.com>
2019-01-06 18:21:26 +0530

    Find the index of smallest element in the list by recursion

---
+++ SmallestElement.py
@@ -0,0 +1,17 @@
+def miniIndex(List,lower,upper):
+    '''
+    OBJECTIVE:TO FIND THE INDEX OF MINIMUM ELEMENT OF THE LIST
+    INPUTS:1.List-LIST INPUTTED BY THE USER
+    RETURN:INDEX OF MINIMUM ELEMENT
+    '''
+    '''
+    APPROACH:BY USING RECURSION
+    '''
+    if lower==upper:
+        return upper
+    elif List[lower]<List[upper]:
+        return miniIndex(List,lower,upper-1)
+    else:
+        return miniIndex(List,lower+1,upper)
+l=[1,6,84,2,4]
+miniIndex(l,0,4)
